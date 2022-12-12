f712d563f3e1526f1cbb66672ee4910985e20456
paupc <p.pulido.c@institutvidreres.cat>
2022-12-12 09:31:30 +0100

    LOGO FET

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
