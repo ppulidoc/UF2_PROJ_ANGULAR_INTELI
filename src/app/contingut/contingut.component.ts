bc43fda5841ca3efdd73448fb6cbd074e0c2306c
nilth <nil.casadesus@institutvidreres.cat>
2022-12-14 09:28:55 +0100

    Commit contingut 1

---
+++ src/app/contingut/contingut.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contingut',
+  templateUrl: './contingut.component.html',
+  styleUrls: ['./contingut.component.css']
+})
+export class ContingutComponent {
+
+}
