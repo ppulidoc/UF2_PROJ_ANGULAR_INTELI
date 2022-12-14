6512ed395957080cd9c3aabeec5aed3385350bde
nilth <nil.casadesus@institutvidreres.cat>
2022-12-14 09:01:12 +0100

    Commit contingut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import {TitolComponent} from "./titol/titol.component";
 import {LogoComponent} from "./logo/logo.component";
+import { ContingutComponent } from './contingut/contingut.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     TitolComponent,
-    LogoComponent
+    LogoComponent,
+    ContingutComponent
   ],
   imports: [
     BrowserModule
