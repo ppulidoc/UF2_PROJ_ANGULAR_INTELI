4a7b932647f8d40a571326f30f47950cc22040e8
paupc <p.pulido.c@institutvidreres.cat>
2022-12-19 08:42:16 +0100

    ASSIDE ENCARA PER MODIFICAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import {TitolComponent} from "./titol/titol.component";
-import {LogoComponent} from "./logo/logo.component";
 import { AssidesComponent } from './assides/assides.component';
+import {ContingutComponent} from "./contingut/contingut.component";
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    TitolComponent,
-    LogoComponent,
-    AssidesComponent
+    AssidesComponent,
+    ContingutComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule
